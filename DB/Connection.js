e168bd815993b069c06d213ae34d577e51a32bae
Saliu J. Okyere-Admin <sokyere-admin@payswitch.com.gh>
2020-11-05 10:23:27 +0000

    changed url

--- DB/Connection.js
+++ DB/Connection.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
+// require('dotenv').config()
 const mongoose = require('mongoose')
 
 
 const connectDb = async()=>{
-   await mongoose.connect(process.env.URL,{useNewUrlParser:true , useUnifiedTopology:true})
+   await mongoose.connect('mongodb+srv://bernyx:bernyx@cluster0.tag3u.mongodb.net/bernyx?retryWrites=true&w=majority',{useNewUrlParser:true , useUnifiedTopology:true})
 
    console.log('Database connected')
    
